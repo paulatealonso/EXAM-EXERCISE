8a2a71dba93a293f85a412e900fb3384b6c0c702
User <paulate.alonso@gmail.com>
2022-11-07 16:30:01 +0100

    Finish

---
+++ EXERCISE A/main.js
@@ -0,0 +1,7 @@
+function chooseNum(num) {
+    for (let i = 0; i <= num; i++) {
+        console.log(i);
+    }
+}
+
+console.log(chooseNum(20));
---
+++ EXERCISE B/main.js
@@ -0,0 +1,7 @@
+function substract(num, num2) {
+    if (num > 0) {
+      return  num -= num2
+    }
+    
+}
+console.log(substract(10,3));
