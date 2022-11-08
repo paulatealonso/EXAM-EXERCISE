352f10a87176c8a3a972b3ce8caa4c2cc713012a
User <paulate.alonso@gmail.com>
2022-11-08 18:34:26 +0100

    ExerciseFinish

--- EXERCISE A/main.js
+++ EXERCISE A/main.js
@@ -1,7 +1,16 @@
-function chooseNum(num) {
-    for (let i = 0; i <= num; i++) {
-        console.log(i);
+
+
+// Usando un bucle for, escribe una función que acepte un número y muestre por consola la lista de números desde el 0 al valor introducido en al función.
+
+function counter(num) {
+    if (typeof(num) === "number") {
+        for (let i = 0; i <= num; i++) {
+            console.log(i);
+        }
+    } else {
+        console.log("Ingresa un numero");
     }
 }
 
-console.log(chooseNum(20));
+counter(20)
+
--- EXERCISE B/main.js
+++ EXERCISE B/main.js
@@ -1,7 +1,21 @@
+
+
+/*Escribe una función que acepte un número y un substractor (otro número). Mientras que el número sea mayor que 0, debes restarle el substractor. Debes devolver el número tal y como ha quedado despues de las restas. No puedes usar un bucle for:
+
+substract(10, 3) // -2
+substract(5, 1) // 0
+*/
+
 function substract(num, num2) {
-    if (num > 0) {
-      return  num -= num2
-    }
-    
+  let counter = 0
+  while (num > 0) {
+  num -= num2
+  counter++  
+  console.log(counter);
+  }
+  console.log(num);
 }
-console.log(substract(10,3));
+
+substract(10, 3);
+
+
